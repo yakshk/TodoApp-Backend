051e0f1e2e5db575e96ef571a80f5f77b8af96fc
Yaksh <kataria.yaksh2@gmail.com>
2026-03-24 23:24:54 +1100

    Added DB logic

--- Infrastructure/Repositories/InMemoryTodoRepository.cs
+++ Infrastructure/Repositories/InMemoryTodoRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace Infrastructure.Repositories;
+﻿using Application.Interfaces;
+using Application.Models;
 
-public class InMemoryTodoRepository
+namespace Infrastructure.Repositories;
+
+public class InMemoryTodoRepository : ITodoRepository
 {
-    
+    private readonly List<TodoItem> _items = [];
+
+    public Task<List<TodoItem>> GetAllAsync()
+    {
+        return Task.FromResult(_items.ToList());
+    }
+
+    public Task<TodoItem?> GetByIdAsync(Guid id)
+    {
+        return Task.FromResult(_items.FirstOrDefault(x => x.Id == id));
+    }
+
+    public Task AddAsync(TodoItem item)
+    {
+        _items.Add(item);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Guid id)
+    {
+        _items.RemoveAll(x => x.Id == id);
+        return Task.CompletedTask;
+    }
 }
